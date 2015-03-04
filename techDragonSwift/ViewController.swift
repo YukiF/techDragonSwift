25becd13ac7e428077bfb2453632c92ff69921af
Yuki.F <drizzle_cold_0904@yahoo.co.jp>
2015-03-04 13:31:44 -0800

    animation

--- techDragonSwift/ViewController.swift
+++ techDragonSwift/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     @IBOutlet var hpLabel : UILabel!
     @IBOutlet var attackLabel : UILabel!
+    @IBOutlet var dragonImage : UIImageView!
     var hpPoint : Int = 100
     var attackPower : Int = 0
     
@@ -25,13 +26,93 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func attackPowerBT() {
+    @IBAction func rasePowerBT() {
         
-        attackPower = attackPower + 10;
+        attackPower = attackPower + 3;
         attackLabel.text = "\(attackPower)"
         
+//        if attackPower >= 10 && attackPower < 30{
+//            attackLabel.textColor = UIColor.blueColor()
+//        }else if attackPower >= 30 && attackPower < 50{
+//            attackLabel.textColor = UIColor.greenColor()
+//        }else if attackPower >= 50{
+//            attackLabel.textColor = UIColor.redColor()
+//        }
+        switch attackPower{
+        case 12:
+            attackLabel.textColor = UIColor.blueColor()
+        case 30:
+            attackLabel.textColor = UIColor.greenColor()
+        case 51:
+            attackLabel.textColor = UIColor.redColor()
+        default:
+            break
+        }
+        
     }
-
-
+    
+    @IBAction func attack(){
+        hpPoint = hpPoint - attackPower
+        if hpPoint <= 0{
+            hpPoint = 0
+            [self .clearAnimation()]
+        }else{
+            [self .dragonAnimation()]
+        }
+        hpLabel.text = "\(hpPoint)"
+    }
+    
+    func dragonAnimation(){
+        UIView.animateWithDuration(0.1, animations: {() -> Void in
+            self.dragonImage.transform = CGAffineTransformMakeScale(0.9, 0.9)
+            }, completion: {(Bool) -> Void in
+                self.dragonImage.transform = CGAffineTransformMakeScale(1.0, 1.0)
+        })
+    }
+    
+    func clearAnimation(){
+        UIView.animateWithDuration(0.7, animations: {() -> Void in
+            self.dragonImage.alpha = 0.0
+            }, completion: {(Bool) -> Void in
+        })
+    }
+    
+    @IBAction func reset(){
+        hpPoint = 100
+        attackPower = 0
+        hpLabel.text = "\(hpPoint)"
+        attackLabel.text = "\(attackPower)"
+        attackLabel.textColor = UIColor.whiteColor()
+        self.dragonImage.alpha = 1.0
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
